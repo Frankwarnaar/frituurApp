7caee4e6991dbb127b13e6be7c02999212d4a43b
Frank Warnaar <Frankwarnaar@gmail.com>
2016-03-18 21:24:27 +0100

    fix imgSrc

--- js/app.js
+++ js/app.js
@@ -4,49 +4,49 @@ var snacks = [
 	time: '4:00',
 	temp: '190',
 	sauce: 'Geen',
-	img: 'moraBitterballen.jpg'},
+	img: 'img/moraBitterballen.jpg'},
 
 	{name: 'Frikandellen',
 	brand: 'Mora',
 	time: '3:37',
 	temp: '190',
 	sauce: 'Mayonaise',
-	img: 'moraFrikandellen.jpg'},
+	img: 'img/moraFrikandellen.jpg'},
 
 	{name: 'Kaassoufles',
 	brand: 'Mora',
 	time: '2:49',
 	temp: '190',
 	sauce: 'Geen',
-	img: 'moraKaassoufle.png'},
+	img: 'img/moraKaassoufle.png'},
 
 	{name: 'Minisnacks',
 	brand: 'Golden',
 	time: '3:37',
 	temp: '190',
 	sauce: 'Mayonaise, Curry',
-	img: 'goldenMinisnacks.jpg'},
+	img: 'img/goldenMinisnacks.jpg'},
 
 	{name: 'Partymix',
 	brand: 'Mora',
 	time: '4:00',
 	temp: '190',
 	sauce: 'Mayonaise, Curry',
-	img: 'moraPartymix.png'},
+	img: 'img/moraPartymix.png'},
 
 	{name: 'Frikandellen',
 	brand: 'Golden',
 	time: '4:00',
 	temp: '190',
 	sauce: 'Mayonaise',
-	img: 'goldenFrikandellen.jpg'},
+	img: 'img/goldenFrikandellen.jpg'},
 
 	{name: 'Vlammetjes',
 	brand: 'TopKing',
 	time: '3:00',
 	temp: '180',
 	sauce: 'Chili saus',
-	img: 'topkingVlammetjes.png'}
+	img: 'img/topkingVlammetjes.png'}
 ];
 
 function addItemToArray(item, array){
@@ -88,6 +88,18 @@ new Vue({
 		clearFilters: function(){
 			this.productFilter = [];
 			this.brandFilter = [];
+		},
+		filterProducts: function(){
+			var allProductFilters = this.productFilter;
+			console.log('test');
 		}
-	}
+	},
+	// filters: {
+	// 	filterProducts: function(){
+	// 		var allProductFilters = this.productFilter;
+	// 		allProductFilters.split ('","');
+			
+	// 	}
+	// }
 });
+

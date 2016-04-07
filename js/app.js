5e10304d2f7771936e356b85a0a8b0b55a407c81
Frank Warnaar <Frankwarnaar@gmail.com>
2016-04-07 14:25:41 +0200

    filters updated

--- js/app.js
+++ js/app.js
@@ -3,51 +3,60 @@ var snacks = [
 	brand: 'Mora',
 	time: '4:00',
 	temp: '190',
-	sauce: 'Geen',
+	sauce: ['Geen'],
 	img: 'img/moraBitterballen.jpg'},
 
 	{name: 'Frikandellen',
 	brand: 'Mora',
 	time: '3:37',
 	temp: '190',
-	sauce: 'Mayonaise',
+	sauce: ['Mayonaise'],
 	img: 'img/moraFrikandellen.jpg'},
 
 	{name: 'Kaassoufles',
 	brand: 'Mora',
 	time: '2:49',
 	temp: '190',
-	sauce: 'Geen',
+	sauce: ['Geen'],
 	img: 'img/moraKaassoufle.png'},
 
 	{name: 'Minisnacks',
 	brand: 'Golden',
 	time: '3:37',
 	temp: '190',
-	sauce: 'Mayonaise, Curry',
+	sauce: ['Mayonaise', 'Curry'],
 	img: 'img/goldenMinisnacks.jpg'},
 
 	{name: 'Partymix',
 	brand: 'Mora',
 	time: '4:00',
 	temp: '190',
-	sauce: 'Mayonaise, Curry',
+	sauce: ['Mayonaise', 'Curry'],
 	img: 'img/moraPartymix.png'},
 
 	{name: 'Frikandellen',
 	brand: 'Golden',
 	time: '4:00',
 	temp: '190',
-	sauce: 'Mayonaise',
+	sauce: ['Mayonaise'],
 	img: 'img/goldenFrikandellen.jpg'},
 
 	{name: 'Vlammetjes',
 	brand: 'TopKing',
 	time: '3:00',
 	temp: '180',
-	sauce: 'Chili saus',
+	sauce: ['Chili saus'],
 	img: 'img/topkingVlammetjes.png'}
 ];
+
+function itemInArray(array, item){
+	for (var i=0; i<array.length; i++){
+		if (item === array[i]){
+			return true
+		}
+	}
+	return false;
+}
 
 function addItemToArray(item, array){
 	var itemInArray = false;
@@ -71,15 +80,30 @@ for (var i=0; i<snacks.length; i++){
 	addItemToArray(snacks[i].brand, allBrands);
 }
 
+var allSauces = [];
+for (var i=0; i<snacks.length; i++){
+	console.log(snacks[i].sauce);
+	for (var a=0; a<snacks[i].sauce.length; a++){
+		console.log(snacks[i].sauce[a]);
+		addItemToArray(snacks[i].sauce[a], allSauces);
+	}
+	// for (var a=0; a<snacks[i].sauce.length; a++){
+	// 	addItemToArray(snacks[i].sauce[a], allSauces);
+	// }
+}
+
 new Vue({
 	el: '#app',
 	data: {
 		snacks: snacks,
 		productFilter: [],
 		brandFilter: [],
+		sauceFilter: [],
 		allProducts: allProducts,
 		allBrands: allBrands,
-		searchTerm: ''
+		allSauces: allSauces,
+		searchTerm: '',
+		productInFilters: true
 	},
 	methods: {
 		clearSearchField: function(){
@@ -88,18 +112,8 @@ new Vue({
 		clearFilters: function(){
 			this.productFilter = [];
 			this.brandFilter = [];
-		},
-		filterProducts: function(){
-			var allProductFilters = this.productFilter;
-			console.log('test');
+			this.sauceFilter = [];
 		}
-	},
-	// filters: {
-	// 	filterProducts: function(){
-	// 		var allProductFilters = this.productFilter;
-	// 		allProductFilters.split ('","');
-			
-	// 	}
-	// }
+	}
 });
 
